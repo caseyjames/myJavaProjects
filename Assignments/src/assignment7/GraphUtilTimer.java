a71aa8efa21499ac4228020056b533439ff120f6
caseyjames <casey.nordgran@gmail.com>
2014-07-06 00:40:24 -0600

    update

--- Assignments/src/assignment7/GraphUtilTimer.java
+++ Assignments/src/assignment7/GraphUtilTimer.java
@@ -1,7 +1,5 @@
 package assignment7;
 
-import assignment5.MyLinkedList;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
@@ -16,8 +14,8 @@ public class GraphUtilTimer {
     }
 
     GraphUtilTimer() {
-        BFStimer();
-//        DFStimer();
+//        BFStimer();
+        DFStimer();
 //        dijkstrasTimer();
 //        topologicalSortTimer();
     }
@@ -90,19 +88,19 @@ public class GraphUtilTimer {
     }
 
     void DFStimer(){
-        int timesToLoop = 20;  // higher number causes more accurate average time, but takes much longer
+        int timesToLoop = 10;  // higher number causes more accurate average time, but takes much longer
         int maxSize = 10000;   // determines right boundary of plot
         Random rand = new Random(); // used to create random lists
 
         //print info for the max input size and the number of times looping, as well as column headers for results
-        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavg");
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime");
 
         // testing loops,each of these loops accounts for a different input size 'N'
         for (int i = 0; i <= maxSize; i += 5000) {
             if (i == 0) i = 1000; // allows i to equal 1000 then 5000 and then even 5000 increments after.
 
             // declare necessary variables, or clear existing ones to start fresh
-            MyLinkedList<Integer> linkedGetList = new MyLinkedList<Integer>();
+            String name1, name2;
             long startTime, midTime, endTime;
             long seed = System.currentTimeMillis();  //use clock as initial starting point for the seed.
             rand.setSeed(seed);
@@ -116,15 +114,31 @@ public class GraphUtilTimer {
             startTime = System.nanoTime();
             rand.setSeed(seed);
             for (int j = 0; j < timesToLoop; j++) {
-                for (int k = 0; k < i; k++) {
-                    linkedGetList.get(rand.nextInt(i));  //'get' will be called i times for timesToLoop times for a
-                }                                           //good average time for i
+                GraphUtil.generateGraphInDotFile("testGraph.dot", i, 2, false, true, false);
+                Graph testGraph = GraphUtil.buildGraphFromDotFile("testGraph.dot");
+                HashMap<String,Vertex> keys = testGraph.getVertices();
+                for (int k = 0; k < 100; k++) {
+                    name1 = "v"; name2 = "v";
+                    while (! keys.containsKey(name1))
+                        name1 = "v" + (i - rand.nextInt(i));
+                    while (! keys.containsKey(name2))
+                        name2 = "v" + (i - rand.nextInt(i));
+                    GraphUtil.depthFirstSearch(testGraph, name1, name2);
+                }
             }
+
             midTime = System.nanoTime();   // midTime is set to aid in subtracting overhead
             rand.setSeed(seed);
             for (int j = 0; j < timesToLoop; j++) {     // same loops run again without .get() to determine overhead
-                for (int k = 0; k < i; k++) {
-                    rand.nextInt(i);
+                GraphUtil.generateGraphInDotFile("testGraph.dot", i, 2, false, true, false);
+                Graph testGraph = GraphUtil.buildGraphFromDotFile("testGraph.dot");
+                HashMap<String,Vertex> keys = testGraph.getVertices();
+                for (int k = 0; k < 100; k++) {
+                    name1 = "v"; name2 = "v";
+                    while (! keys.containsKey(name1))
+                        name1 = "v" + (i - rand.nextInt(i));
+                    while (! keys.containsKey(name2))
+                        name2 = "v" + (i - rand.nextInt(i));
                 }
             }
             endTime = System.nanoTime();
@@ -132,7 +146,7 @@ public class GraphUtilTimer {
             // subtract the over head and determine average time for 'i' calls to get.
             double totalTime = ((midTime - startTime) - (endTime - midTime)) / timesToLoop;
             double avgTime = totalTime / i;
-            System.out.println(i + "\t" + totalTime + "\t" + avgTime);     // print results
+            System.out.println(i + "\t" + avgTime);     // print results
 
             if (i == 1000) i = 0;  // used to allows 1000 to 5000 then even increments of 5000
         }
