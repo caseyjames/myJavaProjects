7190f1463ae38812d762c6d42cc2943d88070053
caseyjames <casey.nordgran@gmail.com>
2014-07-16 12:29:30 -0600

    implement some PriorityQueueHEAP methods

--- Assignments/src/assignment8/HashTableTimer.java
+++ Assignments/src/assignment8/HashTableTimer.java
@@ -107,14 +107,14 @@ public class HashTableTimer {
         return array;
     }
 
-//     TODO: conduct an experiment assess the quality and efficiency of each of your three hash functions.
+//     DO: conduct an experiment assess the quality and efficiency of each of your three hash functions.
 //    * A recommendation for this experiment is to create two plots: one that shows the
 //    growth of the number of collisions incurred by each hash function for a variety of
 //    hash table sizes, and one that shows the actual running time required by each hash
 //    function for a variety of hash table sizes. You may use either type of table for
 //    this experiment.
 
-//     TODO: conduct an experiment assess the quality and efficiency of each of your two hash tables.
+//     DO: conduct an experiment assess the quality and efficiency of each of your two hash tables.
 //    * A recommendation for this experiment is to create two plots: one that shows the number of
 //     collisions incurred by each hash table using the hash function in GoodHashFunctor, and one
 //     that shows the actual running time required by each hash table using the hash function in
