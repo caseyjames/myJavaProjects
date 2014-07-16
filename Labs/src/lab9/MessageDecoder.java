d3bc02dfe3181ed8d465507941bbf5460f042695
caseyjames <casey.nordgran@gmail.com>
2014-07-16 10:20:18 -0600

    implemented lab 9

--- Labs/src/lab9/MessageDecoder.java
+++ Labs/src/lab9/MessageDecoder.java
@@ -10,19 +10,40 @@ import java.io.FileInputStream;
  * @version 7/16/2014
  */
 public class MessageDecoder {
-    // array of bytes to hold input from file
-    byte[] fileData = new byte[376];
 
-    // FileInputStream object to read bytes from the secret.txt file
-    File inputFile;
-    try{
-
-    }
-    FileInputStream inputBytes;
-    try {
-        inputBytes = new FileInputStream(new File("secret.txt"));
-    }
-    catch(Exception e){
-        System.err.println(e.getMessage());
+    public static void main(String[] args) {
+        // array of bytes to hold input from file
+        byte[] fileData = new byte[376];
+        // FileInputStream object to read bytes from the secret.txt file
+        FileInputStream inputBytes;
+        try {
+            inputBytes = new FileInputStream(new File("secret.txt"));
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+            return;
+        }
+        // read enough bytes to fill fileData
+        int feedBack =0;
+        try {
+            feedBack = inputBytes.read(fileData);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        if (feedBack != 376)
+            System.out.println("The correct number of bytes were not retrieved from the file!");
+        // integer to hold 8 bits and convert to a char
+        int nextChar = 0;
+        // String to add characters and hold the final message
+        String secret = "";
+        // obtain key bits and perform algorithm to produce secret message
+        int index = 0;
+        for (int i = 0; i < 47; i++) {
+            for (int j = 0; j < 8; j++)
+                nextChar += ((fileData[index++] >> 2)%2)*((int)Math.pow(2.0, (double)j));
+            secret += (char) nextChar;
+            nextChar = 0;
+        }
+        System.out.println(secret);
     }
 }
