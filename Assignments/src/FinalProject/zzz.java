e572d5cbd698703e284c5ef207957a5bc0de1df9
caseyjames <casey.nordgran@gmail.com>
2014-07-28 20:35:18 -0600

    update

---
+++ Assignments/src/FinalProject/zzz.java
@@ -0,0 +1,13 @@
+package FinalProject;
+
+/**
+ * Created by Casey on 7/28/2014.
+ */
+public class zzz {
+
+    public static void main(String[] args) {
+        String str = "11101001";
+        Byte nbyte = Byte.parseByte(str,2);
+        System.out.println(nbyte);
+    }
+}
